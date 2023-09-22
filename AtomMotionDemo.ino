6ea1b3411db8f1a35fc31cea48fd319c1dc50c49
Adam Howell <AdamJHowell@users.noreply.github.com>
2023-09-21 21:37:28 -0600

    Update AtomMotionDemo.ino

--- AtomMotionDemo.ino
+++ AtomMotionDemo.ino
@@ -19,6 +19,8 @@ DC motor speed range -127~127
 AtomMotion Atom;
 xSemaphoreHandle CtlSemaphore;
 bool direction = true;
+unsigned int speed = 180;
+unsigned int FSM = 0;
 
 
 void GetStatus()
@@ -63,17 +65,18 @@ void setup()
   M5.begin( true, false, true );
   Atom.Init();
   vSemaphoreCreateBinary( CtlSemaphore );
-  xTaskCreatePinnedToCore(
-    // A name just for task.
-    TaskMotion, "TaskMotion"
-    // This stack size can be checked & adjusted by reading the Stack Highwater.
-    , 4096
-    // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    , NULL, 2
-    // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    , NULL, 0 );
+  // xTaskCreatePinnedToCore(
+  //   // A name just for task.
+  //   TaskMotion, "TaskMotion"
+  //   // This stack size can be checked & adjusted by reading the Stack Highwater.
+  //   , 4096
+  //   // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+  //   , NULL, 2
+  //   // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+  //   , NULL, 0 );
     pinMode( PORT_B, INPUT_PULLUP );
     pinMode( PORT_C, INPUT_PULLUP );
+    M5.dis.drawpix( 0, 0xffffff );  // White
 }
 
 
@@ -81,10 +84,47 @@ void loop()
 {
   M5.update();
   if( M5.Btn.wasPressed() )
+  {
     direction = !direction;
+    if( speed == 180 )
+      speed = 0;
+    else
+      speed = 180;
+    Serial.printf( "New speed: %d\n", speed );
+    switch( FSM ) 
+    {
+      case 0:
+          M5.dis.drawpix( 0, 0xfff000 );  // YELLOW 黄色
+          break;
+      case 1:
+          M5.dis.drawpix( 0, 0xff0000 );  // RED  红色
+          break;
+      case 2:
+          M5.dis.drawpix( 0, 0x0000f0 );  // BLUE  蓝色
+          break;
+      case 3:
+          M5.dis.drawpix( 0, 0x00ff00 );  // GREEN  绿色
+          break;
+      default:
+          break;
+    }
+    FSM++;
+    if( FSM >= 4 ) 
+      FSM = 0;
+  }
   if( !digitalRead( PORT_B ) )
+  {
+    Atom.SetServoAngle( 4, 90 );
     Serial.printf( "Hit limit B!\n" );
+  }
+  else 
+    Atom.SetServoAngle( 4, speed );
   if( !digitalRead( PORT_C ) )
+  {
+    Atom.SetServoAngle( 2, 90 );
     Serial.printf( "Hit limit C!\n" );
+  }
+  else 
+    Atom.SetServoAngle( 2, speed );
   delay( 100 );
 }
